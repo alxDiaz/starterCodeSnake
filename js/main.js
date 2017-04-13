dc257963a1ddfa2877ccac8cbfbabc78cf90465e
Alex Diaz <diaz.infante@gmail.com>
2017-04-13 13:14:52 +0200

    Generate food for the snake

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function Game(options){
   this.rows = options.rows;
   this.columns = options.columns;
   this.snake = options.snake;
+  this.food = undefined;
 
   //array that creates the grid where the snake can move.
   for(var rowIndex= 0; rowIndex < this.rows; rowIndex++){
@@ -22,6 +23,7 @@ Game.prototype.drawSnake = function(){
   });
 };
 
+//Before painting the new snake position, we use this method to erase the previous position
 Game.prototype.clearSnake = function(){
   $('.snake').removeClass('snake');
 };
@@ -36,6 +38,36 @@ Game.prototype.update = function(){
   this.drawSnake();
 };
 
+Game.prototype.assignControlsToKeys = function(){
+  $('body').on('keydown', function(e){
+    switch (e.keyCode) {
+      case 37:
+        this.snake.goLeft();
+        break;
+      case 38:
+        this.snake.goUp();
+        break;
+      case 39:
+        this.snake.goRight();
+        break;
+      case 40:
+        this.snake.goDown();
+        break;
+    }
+  }.bind(this));
+};
+
+Game.prototype.generateFood = function(){
+  this.food = {
+    row: Math.floor(Math.random() * this.rows),
+    column: Math.floor(Math.random() * this.columns)
+  };
+};
+
+Game.prototype.drawFood = function(){
+  var selector = '[data-row=' + this.food.row + '][data-column=' + this.food.column + ']';
+  $(selector).addClass('food');
+};
 
 $(document).ready(function(){
   console.log("hola");
@@ -46,5 +78,8 @@ $(document).ready(function(){
   });
 
 //  game.drawSnake();
+  game.assignControlsToKeys();
+  game.generateFood();
+  game.drawFood();
   game.start();
 });
